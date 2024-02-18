b8d30cf08f7ea43a2e7f8190d2376082227996d4
Amal Khardli <amalalikhardli@gmail.com>
2024-02-18 13:55:02 +0300

    update vercel.json

--- dist/middlewares/errorHandler.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.errorHandler = void 0;
-var errorHandler = function (error, req, res, next) {
-    return res.status(error.status || 500).json({
-        massage: error.message,
-    });
-};
-exports.errorHandler = errorHandler;
--- dist/routers/contactRoute.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var express_1 = require("express");
-var contactUsController_1 = require("../controller/contactUsController");
-var router = (0, express_1.Router)();
-router.post('/', contactUsController_1.contactUs);
-exports.default = router;
