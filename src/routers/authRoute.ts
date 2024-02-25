962957f6aba99914fa0ee3c4831ec8cffa971bd7
Amal Khardli <amalalikhardli@gmail.com>
2024-02-25 07:13:59 +0300

    fix last time 13AA

--- src/routers/authRoute.ts
+++ src/routers/authRoute.ts
@@ -1,13 +1,13 @@
-import { Router } from 'express';
-import { rateLimit } from 'express-rate-limit';
+import { Router } from 'express'
+import { rateLimit } from 'express-rate-limit'
 
-import { login, logout } from '../controller/authController';
-import { isLoggedIn, isLoggedOut, userId } from '../middlewares/auth';
-import { loginValidation, userValidation } from '../validation/vaildations';
-import { runValidation } from '../validation/runValidation';
-import { forgetPassword, resetPassword } from '../controller/usersController';
+import { login, logout } from '../controller/authController'
+import { isLoggedIn, isLoggedOut, userId } from '../middlewares/auth'
+import { loginValidation, userValidation } from '../validation/vaildations'
+import { runValidation } from '../validation/runValidation'
+import { forgetPassword, resetPassword } from '../controller/usersController'
 
-const router = Router();
+const router = Router()
 
 // const limiter = rateLimit({
 // 	windowMs: 5 * 60 * 1000, // 5 minutes
@@ -15,9 +15,9 @@ const router = Router();
 //     message: 'You have reached maximum request, please try after 5 minutes'
 // });
 
-router.post('/login', userId, /*limiter*/ isLoggedOut, loginValidation, runValidation, login);
-router.post('/logout',isLoggedIn, logout);
-// forget and reset password 
+router.post('/login', /* userId, limiter isLoggedOut, */ loginValidation, runValidation, login)
+router.post('/logout', logout)
+// forget and reset password
 router.post('/forget-password', isLoggedOut, forgetPassword)
 router.put('/reset-password', isLoggedOut, resetPassword)
-export default router;
+export default router
